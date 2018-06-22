a2275ba73990666273042727132b979ab850ca1e
Lucas Gontijo <Lucasgontijo0@gmail.com>
2018-06-22 16:07:49 -0300

    3 Exercícios.

---
+++ JV-01-Curso-Fundamentos-de-Java/3-Programacao-Java-Conceitos-Iniciais/04-exercicio/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println(fibonacci(i));
+		}
+		
+	}
+
+	public static long fibonacci(int numero) {
+		
+		if (numero < 2) {
+			return numero;
+		}
+		
+		return fibonacci(numero - 1) + fibonacci(numero - 2);
+	}
+}
