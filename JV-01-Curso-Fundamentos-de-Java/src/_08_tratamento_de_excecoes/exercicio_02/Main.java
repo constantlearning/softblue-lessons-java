024f8c7af1dd4cacc46bc46f0535698f955a0d83
Lucas Gontijo <lucasgontijo0@gmail.com>
2018-08-09 13:15:44 -0300

    Exercício 02

---
+++ JV-01-Curso-Fundamentos-de-Java/src/_08_tratamento_de_excecoes/exercicio_02/Main.java
@@ -0,0 +1,9 @@
+package _08_tratamento_de_excecoes.exercicio_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object o = new Object();
+        o.toString();
+    }
+}
